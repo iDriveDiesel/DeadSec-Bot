2d4acc8537da38db47a5d8c485260216cde0779e
iDriveDiesel <jonkellum1@gmail.com>
2026-06-04 13:24:56 -0700

    Update modWatcherConfig by removing mod IDs
    Removed mod IDs from configuration and added collection ID.

--- src/config/modwatcher.js
+++ src/config/modwatcher.js
@@ -2,13 +2,10 @@ export const modWatcherConfig = {
     // How often to check for updates
     pollIntervalMinutes: 5,
 
-    // Steam Workshop mod IDs to watch
-    modIds: [
-        "1559212036", // example
-        "2289461232", // example
-    ],
+    // Steam Workshop Collection ID
+    collectionId: "3669031553",
 
-    // Your Discord webhook URL (we'll set this in a moment)
+    // Your Discord webhook URL
     webhookUrl: "https://discord.com/api/webhooks/1511967617810825216/VSZ-xM90oFoFNVoe9WnHMbg55dzCE_KEl1MqcwMHxXkOKf9TKkKJlnIyrdBKT18zYa0R",
 
     // Where to store last-known timestamps
